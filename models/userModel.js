ed9a37c904eeded7f0332a385915bd9be28fe484
MadgulaVamshi06 <vamshimadgula@gmail.com>
2025-06-16 14:10:16 +0530

    updated model

--- models/userModel.js
+++ models/userModel.js
@@ -5,8 +5,6 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   phone: { type: String, required: true },
-  answer: { type: {}, required: true },
-  address: { type: String, required: true },
   role: { type: Number, default: 0 },
 }, { timestamps: true });
 
